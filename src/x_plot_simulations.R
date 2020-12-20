f18f76c04829f3be0a0f05ee3862c6bf5d6fde3d
Epsian <jared.n.joseph@gmail.com>
2020-12-19 17:11:42 -0800

    fixed the sim process

--- src/x_plot_simulations.R
+++ src/x_plot_simulations.R
@@ -17,9 +17,9 @@ windows(width = 13.889, height = 2.639)
 
 ## in/out ####
 
+t1_pol = readRDS("./data/sims/t1/pol/t1_sim_results_Politician.rda")
 
-
-
+t1_pol_sig = get_att_swap_sig(t1_pol, "Politician")
 
 
 
@@ -31,6 +31,11 @@ windows(width = 13.889, height = 2.639)
 
 
 # make plots ####
+
+nattswap_compare(t1_sim_sig, c("all"), "lep", font_family = "STIX")
+
+
+
 
 nattswap_compare(t1_psim, c("degree", "evc", "closeness", "n_bet"), "Politician", font_family = "STIX", yaxis = c(0, 1250))
 nattswap_compare(t1_lsim, c("degree", "evc", "closeness", "n_bet"), "Law.Enforcement", font_family = "STIX", yaxis = c(0, 1250))
