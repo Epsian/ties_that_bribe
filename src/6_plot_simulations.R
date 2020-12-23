6680705280869fecaaeeb99cdcc71e9db4c32cf9
Epsian <jared.n.joseph@gmail.com>
2020-12-22 21:57:55 -0800

    increase resolution of plots and add number

--- src/6_plot_simulations.R
+++ src/6_plot_simulations.R
@@ -25,10 +25,19 @@ t2_le = readRDS("./data/sims/t2/le/t2_sim_sigs_Law_Enforcement.rda")
 
 # make plots ####
 
+tiff(file="./vis/histogram/t1_pol.tiff", width = 13.889 * 200, height = 2.639 * 200, units = "px", pointsize = 20, res = 200, family = "STIX", compression = "none")
 nattswap_compare(t1_pol, c("degree", "evc", "nestedness"), "Politician", font_family = "STIX", yaxis = c(0, 1250))
+dev.off()
+
+tiff(file="./vis/histogram/t1_le.tiff", width = 13.889 * 200, height = 2.639 * 200, units = "px", pointsize = 20, res = 200, family = "STIX", compression = "none")
 nattswap_compare(t1_le, c("degree", "evc", "nestedness"), "Law.Enforcement", font_family = "STIX", yaxis = c(0, 1250))
+dev.off()
 
+tiff(file="./vis/histogram/t2_pol.tiff", width = 13.889 * 200, height = 2.639 * 200, units = "px", pointsize = 20, res = 200, family = "STIX", compression = "none")
 nattswap_compare(t2_pol, c("degree", "evc", "nestedness"), "Politician", font_family = "STIX", yaxis = c(0, 1250))
-nattswap_compare(t2_le, c("degree", "evc", "nestedness"), "Law.Enforcement", font_family = "STIX", yaxis = c(0, 1250))
+dev.off()
 
+tiff(file="./vis/histogram/t2_le.tiff", width = 13.889 * 200, height = 2.639 * 200, units = "px", pointsize = 20, res = 200, family = "STIX", compression = "none")
+nattswap_compare(t2_le, c("degree", "evc", "nestedness"), "Law.Enforcement", font_family = "STIX", yaxis = c(0, 1250))
+dev.off()
 
