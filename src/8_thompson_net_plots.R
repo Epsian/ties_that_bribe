0acf29691a92b9ede7dc0988daba6404d844eeeb
Epsian <jared.n.joseph@gmail.com>
2021-01-09 17:25:14 -0800

    degree plot working

--- src/8_thompson_net_plots.R
+++ src/8_thompson_net_plots.R
@@ -38,131 +38,60 @@ t2_data[t2_data$politician == TRUE, "Node Type"] = "Politician"
 # make barcharts ####
 
 # make df to hold results
-billdf = data.frame("group" = c("bill", "all", "lep", "le", "pol", "crim"), stringsAsFactors = FALSE)
+billdf = data.frame("group" = c("Thompson", "Law Enforcement", "Politician", "Criminal"), stringsAsFactors = FALSE)
 
 # degree
-billdf$degree = c(t2_data$degree[t2_data$vertex_name == "Thompson, Big Bill"], mean(t2_data$degree), mean(t2_data$degree[t2_data$lep]), mean(t2_data$degree[t2_data$law_enforcement]), mean(t2_data$degree[t2_data$politician]), mean(t2_data$degree[!(t2_data$politician | t2_data$law_enforcement)]))
+billdf$degree = c(t2_data$degree[t2_data$vertex_name == "Thompson, Big Bill"], mean(t2_data$degree[t2_data$law_enforcement]), mean(t2_data$degree[t2_data$politician]), mean(t2_data$degree[!(t2_data$politician | t2_data$law_enforcement)]))
 
 # eigenvectors
-billdf$evc = c(t2_data$evc[t2_data$vertex_name == "Thompson, Big Bill"], mean(t2_data$evc), mean(t2_data$evc[t2_data$lep]), mean(t2_data$evc[t2_data$law_enforcement]), mean(t2_data$evc[t2_data$politician]), mean(t2_data$evc[!(t2_data$politician | t2_data$law_enforcement)]))
+billdf$evc = c(t2_data$evc[t2_data$vertex_name == "Thompson, Big Bill"], mean(t2_data$evc[t2_data$law_enforcement]), mean(t2_data$evc[t2_data$politician]), mean(t2_data$evc[!(t2_data$politician | t2_data$law_enforcement)]))
 
 # nestedness
-billdf$nestedness = c(t2_data$nestedness[t2_data$vertex_name == "Thompson, Big Bill"], mean(t2_data$nestedness), mean(t2_data$nestedness[t2_data$lep]), mean(t2_data$nestedness[t2_data$law_enforcement]), mean(t2_data$nestedness[t2_data$politician]), mean(t2_data$nestedness[!(t2_data$politician | t2_data$law_enforcement)]))
+billdf$nestedness = c(t2_data$nestedness[t2_data$vertex_name == "Thompson, Big Bill"], mean(t2_data$nestedness[t2_data$law_enforcement]), mean(t2_data$nestedness[t2_data$politician]), mean(t2_data$nestedness[!(t2_data$politician | t2_data$law_enforcement)]))
 
-# plot
-ggplot(billdf) +
-  geom_dotplot(aes(x = group, y = degree)) +
-  ylim(0, 1)
+# plot ####
 
+# reshape df
+bill_melt = reshape2::melt(billdf)
 
-
-dotchart(as.matrix(billdf[,2:3]), labels = billdf$group)
-
-
-ggplot(billdf, aes(x=dose, y=len)) + 
-  geom_dotplot(binaxis='y', stackdir='center', fill="#FFAAD4")
-
-test = reshape2::melt(billdf)
-
-
-
-ggplot(test) +
-  geom_dotplot(aes(x = variable, y = value, fill = group))
-
-
-
-#x is degree, evc, nestedn
-#y is nu,
-#color is group
-
-
-ggplot(test[test$variable == "degree",], aes(x=variable, y=value)) +
+degree_plot = ggplot(bill_melt[bill_melt$variable == "degree",], aes(x=variable, y=value)) +
+  geom_segment(aes(x=variable, xend=variable, y=min(value), yend=max(value)), linetype="dashed", size=0.1) +   # Draw dashed lines
   geom_point(aes(col=group), size=3) +   # Draw points
+  scale_color_manual(breaks = c("Criminal", "Law Enforcement", "Politician", "Thompson"), values = c(crim.col, le.col, pol.col, "red")) +
+  labs(title="Degree", subtitle=NULL, color = "Node Type") + xlab(NULL) + ylab("Value") +
+  theme_classic(base_size = 12, base_family = "STIX") + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) + coord_flip()
+
+
+
+
+
+
+
+evc_plot = ggplot(bill_melt[bill_melt$variable == "evc",], aes(x=variable, y=value)) +
   geom_segment(aes(x=variable, 
                    xend=variable, 
                    y=min(value), 
                    yend=max(value)), 
                linetype="dashed", 
                size=0.1) +   # Draw dashed lines
-  labs(title="Dot Plot", 
-       subtitle="Make Vs Avg. Mileage", 
-       caption="source: mpg") +  
-  coord_flip()
-
-ggplot(test[test$variable == "evc",], aes(x=variable, y=value)) +
   geom_point(aes(col=group), size=3) +   # Draw points
-  geom_segment(aes(x=variable, 
-                   xend=variable, 
-                   y=min(value), 
-                   yend=max(value)), 
-               linetype="dashed", 
-               size=0.1) +   # Draw dashed lines
-  labs(title="Dot Plot", 
-       subtitle="Make Vs Avg. Mileage", 
-       caption="source: mpg") +  
-  coord_flip()
-
-
-
-ggplot(test, aes(x=variable, y=value)) + 
-  geom_point(aes(col=group), size=3) +   # Draw points
-  geom_segment(aes(x=variable, 
-                   xend=variable, 
-                   y=min(value), 
-                   yend=max(value)), 
-               linetype="dashed", 
-               size=0.1) +   # Draw dashed lines
-  labs(title="Dot Plot", 
-       subtitle="Make Vs Avg. Mileage", 
-       caption="source: mpg") +  
-  coord_flip() +
-  facet_grid(variable ~ .)
-
-
-
-
-
-
-
-
-
-
-
-df <- data.frame(City = c("AMS", "AMS", "AMS", "AMS", "BEL", "BEL", "BEL", "BEL"),
-                 Month = c(4, 5, 6, 7, 4, 5, 6, 7),
-                 Ratio = c(8, 9, 10, 5, 12, 13, 9, 10))
-
-dp <- ggplot(df) + geom_dotplot(aes(x = Month, y = Ratio, fill = City), position = "dodge") 
-dp
-
-ggplot(df) + geom_dotplot(aes(x = Month, Ratio))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-t1_dn_plot = ggplot(t1_data, aes(x = degree, y = nestedness, color = `Node Type`)) +
-  geom_jitter(width = 1, height = 1, size = 1) +
-  labs(title = "Nestedness by Degree Centrality", subtitle = "1900-1919") +
+  scale_color_manual(breaks = c("Criminal", "Law Enforcement", "Politician", "Thompson"), values = c(crim.col, le.col, pol.col, "red")) +
+  labs(title="Degree", subtitle=NULL) + 
   theme_classic(base_size = 12, base_family = "STIX") +
-  scale_color_manual(breaks = c("Criminal", "Law Enforcement", "Politician"), values = c(crim.col, le.col, pol.col)) +
-  stat_smooth(method = lm, se = FALSE, size = .4, linetype = "dotted", fullrange = TRUE) +
-  xlim(0, 70) + 
-  ylim(0, 35)
+  coord_flip()
+
+nestedness_plot = ggplot(bill_melt[bill_melt$variable == "nestedness",], aes(x=variable, y=value)) +
+  geom_segment(aes(x=variable, 
+                   xend=variable, 
+                   y=min(value), 
+                   yend=max(value)), 
+               linetype="dashed", 
+               size=0.1) +   # Draw dashed lines
+  geom_point(aes(col=group), size=3) +   # Draw points
+  scale_color_manual(breaks = c("Criminal", "Law Enforcement", "Politician", "Thompson"), values = c(crim.col, le.col, pol.col, "red")) +
+  labs(title="Degree", subtitle=NULL) + 
+  theme_classic(base_size = 12, base_family = "STIX") +
+  coord_flip()
+
+
+
