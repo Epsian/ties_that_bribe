db1e0440d975498616ebd202b1e98bde36f43a92
Epsian <jared.n.joseph@gmail.com>
2021-01-09 18:59:27 -0800

    dotplots work; now work on egonet

--- src/8_thompson_net_plots.R
+++ src/8_thompson_net_plots.R
@@ -7,6 +7,7 @@ library(ggplot2)
 library(ggthemes)
 library(plotly)
 library(htmlwidgets)
+library(ggpubr)
 
 # add journal font
 library(showtext)
@@ -35,7 +36,7 @@ t2_data$`Node Type` = "Criminal"
 t2_data[t2_data$law_enforcement == TRUE, "Node Type"] = "Law Enforcement"
 t2_data[t2_data$politician == TRUE, "Node Type"] = "Politician"
 
-# make barcharts ####
+# make dotplots ####
 
 # make df to hold results
 billdf = data.frame("group" = c("Thompson", "Law Enforcement", "Politician", "Criminal"), stringsAsFactors = FALSE)
@@ -49,49 +50,49 @@ billdf$evc = c(t2_data$evc[t2_data$vertex_name == "Thompson, Big Bill"], mean(t2
 # nestedness
 billdf$nestedness = c(t2_data$nestedness[t2_data$vertex_name == "Thompson, Big Bill"], mean(t2_data$nestedness[t2_data$law_enforcement]), mean(t2_data$nestedness[t2_data$politician]), mean(t2_data$nestedness[!(t2_data$politician | t2_data$law_enforcement)]))
 
-# plot ####
-
 # reshape df
 bill_melt = reshape2::melt(billdf)
 
-degree_plot = ggplot(bill_melt[bill_melt$variable == "degree",], aes(x=variable, y=value)) +
+# degree plot
+degree_plot = ggplot(bill_melt[bill_melt$variable == "degree" & bill_melt$group != "Thompson",], aes(x=variable, y=value)) +
   geom_segment(aes(x=variable, xend=variable, y=min(value), yend=max(value)), linetype="dashed", size=0.1) +   # Draw dashed lines
-  geom_point(aes(col=group), size=3) +   # Draw points
-  scale_color_manual(breaks = c("Criminal", "Law Enforcement", "Politician", "Thompson"), values = c(crim.col, le.col, pol.col, "red")) +
+  geom_point(aes(col=group), size=5) +   # Draw points
+  scale_color_manual(breaks = c("Criminal", "Law Enforcement", "Politician"), values = c(crim.col, le.col, pol.col)) +
   labs(title="Degree", subtitle=NULL, color = "Node Type") + xlab(NULL) + ylab("Value") +
+  geom_hline(yintercept = 7, size = 1) + # 7 is bill's degree
   theme_classic(base_size = 12, base_family = "STIX") + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) + coord_flip()
 
+degree_plot
 
+# eigenvector
+evc_plot = ggplot(bill_melt[bill_melt$variable == "evc" & bill_melt$group != "Thompson",], aes(x=variable, y=value)) +
+  geom_segment(aes(x=variable, xend=variable, y=min(value), yend=0.026289433), linetype="dashed", size=0.1) +   # Draw dashed lines
+  geom_point(aes(col=group), size=5) +   # Draw points
+  scale_color_manual(breaks = c("Criminal", "Law Enforcement", "Politician"), values = c(crim.col, le.col, pol.col)) +
+  labs(title="Eigenvector", subtitle=NULL, color = "Node Type") + xlab(NULL) + ylab("Value") +
+  geom_hline(yintercept = 0.026289433, size = 1) + # 0.026289433 is bill's evc
+  theme_classic(base_size = 12, base_family = "STIX") + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) + coord_flip()
 
+evc_plot
 
+# nestedness
+nest_plot = ggplot(bill_melt[bill_melt$variable == "nestedness" & bill_melt$group != "Thompson",], aes(x=variable, y=value)) +
+  geom_segment(aes(x=variable, xend=variable, y=min(value), yend=max(value)), linetype="dashed", size=0.1) +   # Draw dashed lines
+  geom_point(aes(col=group), size=5) +   # Draw points
+  scale_color_manual(breaks = c("Criminal", "Law Enforcement", "Politician"), values = c(crim.col, le.col, pol.col)) +
+  labs(title="Nestedness", subtitle=NULL, color = "Node Type") + xlab(NULL) + ylab("Value") +
+  geom_hline(yintercept = 4, size = 1) + # 4 is bill's nestedness
+  theme_classic(base_size = 12, base_family = "STIX") + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) + coord_flip()
 
+nest_plot
 
+# combine
+multidot = ggarrange(degree_plot, evc_plot, nest_plot, ncol = 1, nrow = 3)
+multidot
+ggsave("./vis/thompson_plots/dotplot.pdf", multidot, scale = 1, width = 7.18, height = 4)
 
-evc_plot = ggplot(bill_melt[bill_melt$variable == "evc",], aes(x=variable, y=value)) +
-  geom_segment(aes(x=variable, 
-                   xend=variable, 
-                   y=min(value), 
-                   yend=max(value)), 
-               linetype="dashed", 
-               size=0.1) +   # Draw dashed lines
-  geom_point(aes(col=group), size=3) +   # Draw points
-  scale_color_manual(breaks = c("Criminal", "Law Enforcement", "Politician", "Thompson"), values = c(crim.col, le.col, pol.col, "red")) +
-  labs(title="Degree", subtitle=NULL) + 
-  theme_classic(base_size = 12, base_family = "STIX") +
-  coord_flip()
+# make egonet ####
 
-nestedness_plot = ggplot(bill_melt[bill_melt$variable == "nestedness",], aes(x=variable, y=value)) +
-  geom_segment(aes(x=variable, 
-                   xend=variable, 
-                   y=min(value), 
-                   yend=max(value)), 
-               linetype="dashed", 
-               size=0.1) +   # Draw dashed lines
-  geom_point(aes(col=group), size=3) +   # Draw points
-  scale_color_manual(breaks = c("Criminal", "Law Enforcement", "Politician", "Thompson"), values = c(crim.col, le.col, pol.col, "red")) +
-  labs(title="Degree", subtitle=NULL) + 
-  theme_classic(base_size = 12, base_family = "STIX") +
-  coord_flip()
 
 
 
