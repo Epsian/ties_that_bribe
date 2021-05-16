0d6d196a2990709da423035ed3c8e11da884a94b
Jared Joseph <jared.n.joseph@gmail.com>
2021-05-15 19:00:12 -0700

    standardized x axes per time

--- src/6_plot_simulations.R
+++ src/6_plot_simulations.R
@@ -24,27 +24,33 @@ t2_pol = readRDS("./data/sims/t2/pol/t2_sim_sigs_Politician.rda")
 t2_le = readRDS("./data/sims/t2/le/t2_sim_sigs_Law_Enforcement.rda")
 
 # make x axis settings list ####
-xaxis_settings = list(
-  "degree" = c(0, 25),
+t1_xaxis_settings = list(
+  "degree" = c(2, 16),
   "evc" = c(0, .1),
-  "nestedness" = c(0,8)
+  "nestedness" = c(1.5,7)
+)
+
+t2_xaxis_settings = list(
+  "degree" = c(2.5, 25),
+  "evc" = c(0, .05),
+  "nestedness" = c(1,8)
 )
 
 # make plots ####
 
 pdf(file="./vis/histogram/t1_pol.pdf", height = 2.639, width = 13.889)
-nattswap_compare(t1_pol, c("degree", "evc", "nestedness"), "Politician", font_family = "STIX", yaxis = c(0, 1250))
+nattswap_compare(t1_pol, c("degree", "evc", "nestedness"), "Politician", font_family = "STIX", yaxis = c(0, 1250), xaxis = t1_xaxis_settings, bin_num = 60)
 dev.off()
 
 pdf(file="./vis/histogram/t1_le.pdf", width = 13.889, height = 2.639)
-nattswap_compare(t1_le, c("degree", "evc", "nestedness"), "Law.Enforcement", font_family = "STIX", yaxis = c(0, 1250))
+nattswap_compare(t1_le, c("degree", "evc", "nestedness"), "Law.Enforcement", font_family = "STIX", yaxis = c(0, 1250), xaxis = t1_xaxis_settings, bin_num = 60)
 dev.off()
 
 pdf(file="./vis/histogram/t2_pol.pdf", width = 13.889, height = 2.639)
-nattswap_compare(t2_pol, c("degree", "evc", "nestedness"), "Politician", font_family = "STIX", yaxis = c(0, 1250))
+nattswap_compare(t2_pol, c("degree", "evc", "nestedness"), "Politician", font_family = "STIX", yaxis = c(0, 1250), xaxis = t2_xaxis_settings, bin_num = 60)
 dev.off()
 
 pdf(file="./vis/histogram/t2_le.pdf", width = 13.889, height = 2.639)
-nattswap_compare(t2_le, c("degree", "evc", "nestedness"), "Law.Enforcement", font_family = "STIX", yaxis = c(0, 1250))
+nattswap_compare(t2_le, c("degree", "evc", "nestedness"), "Law.Enforcement", font_family = "STIX", yaxis = c(0, 1250), xaxis = t2_xaxis_settings, bin_num = 60)
 dev.off()
 
